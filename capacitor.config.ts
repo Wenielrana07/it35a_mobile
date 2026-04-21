e0507e192598115fe28ecfba7e4406fb50fbfa13
Wenielrana07 <rana.weniel@gmail.com>
2026-04-21 11:23:29 +0800

    Add files via upload

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'it35-a-mobile',
+  appName: 'it35a-mobile',
   webDir: 'dist'
 };
 
